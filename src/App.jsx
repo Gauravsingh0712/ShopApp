b6e04832fa015ce088a7807c63c340eab1ee4149
Gaurav Singh <sumitgaurav6295@gmail.com>
2024-03-18 22:22:21 +0530

    started with shop app

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// import { Routes } from "react-router-dom";
+// import Navbar from "./components/Navbar";
+// import { Route } from "react-router-dom";
+// import Home from "./pages/Home";
+// import Cart from "./pages/Cart";
+
+const App = () => {
+  return <div>shopping cart</div>;
+};
+
+export default App;
